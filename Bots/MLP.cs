7a055bbf0ee673a7e368d626dea5a062e28443c8
Adwait Creationz <adwait.34@gmail.com>
2021-08-24 16:42:25 +0530

    Added some more structure i guess

--- Bots/MLP.cs
+++ Bots/MLP.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace AdwintCS
+namespace AdwintCS.Bots
 {
     public class MLP
     {
---
+++ DataStru/MLPLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdwintCS.DataStru
+{
+    class MLPLayer
+    {
+        int inlen;
+        float[] Biases;
+        MLPLayer(int inputs,int length)
+        {
+            inlen = inputs;
+            Biases = new float[length];
+        }
+    }
+}
